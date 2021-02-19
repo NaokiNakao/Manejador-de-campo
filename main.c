d09c13d2b0c66da6ddf9050a5e8a848b42dde07d
Naoki Nakao <naokinakao@gmail.com>
2021-02-19 16:10:55 -0500

    Correcciones iniciales

--- main.c
+++ main.c
@@ -1,16 +1,85 @@
 /*
-   Manejador de campo.
+   Manejador de campo programado en C.
 
    Autor : Naoki Nakao
    Fecha :
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.c>
+
 /* Macros */
+
+#define TRUE    1
+#define FALSE   0
+#define MAX    80
+
+#define RIGHT   77
+#define LEFT    75
+#define ENTER   13
+#define ESC     27
+#define BKSP     8
+#define SPACE   32
+
+#define INI_X    1
+#define INI_Y    1
 
 /* Prototipos de función */
 
 int main()
 {
 
+
    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
